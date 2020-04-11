ffbd4f35a95247d6e7bc82f1e454dbf78835c5be
Yuri Alves <yurialvesguernsey@outlook.com>
2020-04-12 00:19:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $authValid = false;
 $testUsername = "Yuri";
 $testPassword =  "Pass";
 ?>
-<?php if($_POST['username'] === $testUsername && $_POST['password'] === $testPassword): return $authValid = true; ?>
-<?php else: return $authValid = false;?>
+<?php if($_POST['username'] === $testUsername && $_POST['password'] === $testPassword): echo $authValid = true; ?>
+<?php else: echo $authValid = false;?>
 
 <?php endif; ?> 
