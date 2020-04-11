d41cf346412cb1ae80d85636a13f23bd884fce70
Yuri Alves <yurialvesguernsey@outlook.com>
2020-04-12 00:44:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $testUsername = "Yuri";
 $testPassword =  "Pass";
 ?>
-<?php if($_POST['username'] === $testUsername && $_POST['password'] === $testPassword): echo ('Login'); ?>
-<?php else: echo ('error')?>
+<?php if($_POST['username'] === $testUsername && $_POST['password'] === $testPassword): echo 'Login'; ?>
+<?php else: echo 'error' ?>
 
 <?php endif; ?> 
